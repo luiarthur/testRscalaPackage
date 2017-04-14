7b43d3db8de817f9b55054e714023d01027d28d1
Arthur Lui <luiarthur@gmail.com>
2017-04-13 18:27:47 -0700

    yes

--- R/hello.R
+++ R/hello.R
@@ -3,7 +3,8 @@ hello <- function(x) {
   #' @export
   print("I am HERE")
   
-  rscala::scalaEval(s,'val out = hello.Hello.hello("asd")')
+  #rscala::scalaEval(s,'val out = Hello.hello("asd")')
+  rscala::scalaEval(s,'val out = "xy"')
   rscala::scalaGet(s,"out")
   out
 }
