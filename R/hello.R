1bc66be4f9d1942605ff16dc880b0711c373efb4
Arthur Lui <luiarthur@gmail.com>
2017-04-13 21:24:29 -0700

    yes

--- R/hello.R
+++ R/hello.R
@@ -1,13 +1,6 @@
 hello <- function(x) {
-  #' hello
   #' @export
-  print("I am HERE")
 
-  # This works
-  #rscala::scalaEval(s,'val out = "xyz"')
-  
-  # This doesn't work?!
-  rscala::scalaEval(s,paste0('val out = Hello.hello("',x,'")'))
-
+  rscala::scalaEval(s,paste0('val out = Hello.hello("@{x}")'))
   return(rscala::scalaGet(s,"out"))
 }
