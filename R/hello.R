6ab48c61cdf2df4f33111bf42c9d80a6704e8aaa
Arthur Lui <luiarthur@gmail.com>
2017-04-13 21:33:45 -0700

    yes

--- R/hello.R
+++ R/hello.R
@@ -2,7 +2,5 @@ hello <- function(x) {
   #' hello
   #' @export
 
-  #rscala::scalaEval(s,paste0('val out = Hello.hello("@{x}")'))
-  #return(rscala::scalaGet(s,"out"))
-  rscala::`%~%`(testRscalaPackage::s,'Hello.hello("@{x}")')
+  rscala::`%~%`(s,'Hello.hello("@{x}")')
 }
