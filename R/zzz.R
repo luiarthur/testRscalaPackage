eb4cd9379de8f01b92fa9ea5cbd01a17b1a700ff
Arthur Lui <luiarthur@gmail.com>
2017-04-15 11:12:28 -0700

    yes

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   #print("Loading Hello")  
-  print(find.package(libname))
+  #print(find.package(libname))
   print(find.package(pkgname))
   rscala::.rscalaPackage(pkgname)
 }
